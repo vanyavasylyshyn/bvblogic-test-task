561a33ba18c43e70a9c0339be0c17e15230ab3d0
vanyavasylyshyn <vanyavasylyshyn@gmail.com>
2019-07-11 19:16:49 +0300

    Created house creating endpoint and method

--- src/house/house.controller.ts
+++ src/house/house.controller.ts
@@ -1,10 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 
 import { HouseService } from './house.service';
+
+import { createHouseDto } from './dto/createHouse.dto';
 
 @Controller('house')
 export class HouseController {
   constructor(private readonly houseService: HouseService) { }
 
+  @Post('create')
+  async create(@Body() houseData: createHouseDto): Promise<any> {
+    return await this.houseService.create(houseData);
+  }
   
 }
--- src/house/house.service.ts
+++ src/house/house.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
@@ -6,6 +6,8 @@ import { HouseEntity } from '../entity/houseInfo.entity';
 import { PeriodsEntity } from '../entity/periods.entity';
 import { BlockedEntity } from '../entity/blocked.entity';
 import { BookingEntity } from '../entity/booking.entity';
+
+import { createHouseDto } from './dto/createHouse.dto';
 
 
 
@@ -22,6 +24,51 @@ export class HouseService {
     private readonly bookingRepository: Repository<BookingEntity>,
   ) { }
 
+  //
+  //
+  //Creating house
+  //
+  //
   
+  async create(dto: createHouseDto): Promise<any> {
+
+    let house = await this.houseRepository.findOne({
+      where: [{ name: dto.name },
+      { adress: dto.adress }]
+    });
+
+    if (house) {
+      const errors = { message: 'House or adress already exist.' };
+      throw new HttpException({ message: 'Input data validation failed.', errors }, HttpStatus.BAD_REQUEST);
+    }
+
+    house = new HouseEntity();
+    house.name = dto.name;
+    house.description = dto.description;
+    house.minDuration = dto.minDuration;
+    house.adress = dto.adress;
+    house.dailyPrice = dto.dailyPrice;
+    house.weekendPrice = dto.weekendPrice;
+    house.discount = dto.discount;
+
+
+    if (dto.blockedStart && dto.blockedFinish) {
+      let period = new PeriodsEntity();
+      period.startingDate = dto.blockedStart;
+      period.finishingDate = dto.blockedFinish;
+
+      let blocked = new BlockedEntity();
+      blocked.period = period;
+      blocked.house = house;
+      blocked.house = house;
+      blocked.period = period;
+
+      await this.houseRepository.save(house);
+
+      return await this.blockedRepository.save(blocked);
+    } else {
+      return await this.houseRepository.save(house);
+    }
+  };
 
 }
