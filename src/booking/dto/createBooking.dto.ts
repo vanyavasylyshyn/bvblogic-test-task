2703576ec9763f61a290f2856fd44990d0acc8bc
vanyavasylyshyn <vanyavasylyshyn@gmail.com>
2019-07-11 19:48:11 +0300

    Created createBooking data transfer object

---
+++ src/booking/dto/createBooking.dto.ts
@@ -0,0 +1,7 @@
+export class createBookingDto {
+  readonly startingDate: Date;
+  readonly finishingDate: Date;
+  readonly name: string;
+  readonly surname: string;
+  readonly phonenumber: string;
+}
